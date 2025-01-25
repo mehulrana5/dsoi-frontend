79367322b95722c91f01a5e583a9233c5a74417e
mehul <cs20077@glbitm.ac.in>
2025-01-26 02:33:45 +0530

    feat: add Popover and Calendar components; update package.json and package-lock.json with new dependencies

--- src/pages/AddGuestPage.tsx
+++ src/pages/AddGuestPage.tsx
@@ -1,15 +1,207 @@
+"use client"
+import {
+    toast
+} from "sonner"
+import {
+    useForm
+} from "react-hook-form"
+import {
+    zodResolver
+} from "@hookform/resolvers/zod"
+import * as z from "zod"
+import {
+    cn
+} from "@/lib/utils"
+import {
+    Button
+} from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {
+    Input
+} from "@/components/ui/input"
+import {
+    format
+} from "date-fns"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger
+} from "@/components/ui/popover"
+import {
+    Calendar
+} from "@/components/ui/calendar"
+import {
+    Calendar as CalendarIcon
+} from "lucide-react"
+
+const formSchema = z.object({
+    name_7230181643: z.string().min(0),
+    name_3724634255: z.coerce.date(),
+    name_7811330857: z.coerce.date()
+});
+
 function AddGuestPage() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            "name_3724634255": new Date(),
+            "name_7811330857": new Date()
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        try {
+            console.log(values);
+            toast(
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(values, null, 2)}</code>
+                </pre>
+            );
+        } catch (error) {
+            console.error("Form submission error", error);
+            toast.error("Failed to submit the form. Please try again.");
+        }
+    }
+
     return (
         <div style={{
             display: 'flex',
+            flexDirection: "column",
             flexWrap: "wrap",
             padding: "1em",
             alignItems: "center",
             justifyContent: "center",
             maxWidth: "1300px",
-            margin: "auto"
+            margin: "auto",
         }}>
             <h1>Add Guest Page</h1>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 max-w-3xl mx-auto py-10">
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+
+                        <div className="col-span-1 lg:col-span-1">
+
+                            <FormField
+                                control={form.control}
+                                name="name_7230181643"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Guest Name *</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Name"
+                                                type="text"
+                                                {...field} />
+                                        </FormControl>
+
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                        <div className="col-span-1">
+
+                            <FormField
+                                control={form.control}
+                                name="name_3724634255"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Arrival *</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-full md:w-[240px] pl-3 text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>When to activate the guest pass</FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="col-span-1">
+                            <FormField
+                                control={form.control}
+                                name="name_7811330857"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel>Departure *</FormLabel>
+                                        <Popover>
+                                            <PopoverTrigger asChild>
+                                                <FormControl>
+                                                    <Button
+                                                        variant={"outline"}
+                                                        className={cn(
+                                                            "w-full md:w-[240px] pl-3 text-left font-normal",
+                                                            !field.value && "text-muted-foreground"
+                                                        )}
+                                                    >
+                                                        {field.value ? (
+                                                            format(field.value, "PPP")
+                                                        ) : (
+                                                            <span>Pick a date</span>
+                                                        )}
+                                                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                    </Button>
+                                                </FormControl>
+                                            </PopoverTrigger>
+                                            <PopoverContent className="w-auto p-0" align="start">
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={field.onChange}
+                                                    initialFocus
+                                                />
+                                            </PopoverContent>
+                                        </Popover>
+                                        <FormDescription>When to deactivate the guest pass</FormDescription>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                    </div>
+                    <Button type="submit" className="w-full md:w-auto">Submit</Button>
+                </form>
+            </Form>
         </div>
     )
 }
