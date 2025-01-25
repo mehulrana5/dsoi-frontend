8e85e3b6cc6345f905c8a0c78b110f52c5087a02
mehul <cs20077@glbitm.ac.in>
2025-01-26 01:52:59 +0530

    refactor: simplify TransactionsPage by removing unused TransactionTable component and optimizing data loading

--- src/pages/TransactionsPage.tsx
+++ src/pages/TransactionsPage.tsx
@@ -25,44 +25,6 @@ interface Transaction {
     amount: number;
 }
 
-function TransactionTable() {
-    const data: Transaction[] = [
-        { id: 1, dateTime: "2023-10-01 10:00", type: "Deduct", amount: 100 },
-        { id: 2, dateTime: "2023-10-02 11:30", type: "Deduct", amount: 50 },
-        { id: 3, dateTime: "2023-10-03 14:45", type: "Recharge", amount: 200 },
-        { id: 4, dateTime: "2023-10-04 09:20", type: "Deduct", amount: 75 },
-        { id: 5, dateTime: "2023-10-05 16:10", type: "Recharge", amount: 150 },
-        { id: 6, dateTime: "2023-10-06 12:00", type: "Deduct", amount: 60 },
-        { id: 7, dateTime: "2023-10-07 08:30", type: "Recharge", amount: 300 },
-        { id: 8, dateTime: "2023-10-08 14:00", type: "Deduct", amount: 90 },
-        { id: 9, dateTime: "2023-10-09 11:15", type: "Recharge", amount: 250 },
-        { id: 10, dateTime: "2023-10-10 10:45", type: "Deduct", amount: 40 },
-        { id: 11, dateTime: "2023-10-11 13:30", type: "Recharge", amount: 220 },
-        { id: 12, dateTime: "2023-10-12 15:50", type: "Deduct", amount: 110 },
-        { id: 13, dateTime: "2023-10-13 09:40", type: "Recharge", amount: 180 },
-    ];
-    return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Date Time</TableHead>
-                    <TableHead>Type</TableHead>
-                    <TableHead>Amount</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {data.map((transaction) => (
-                    <TableRow key={transaction.id}>
-                        <TableCell>{transaction.dateTime}</TableCell>
-                        <TableCell>{transaction.type}</TableCell>
-                        <TableCell>{transaction.amount}</TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
-    );
-}
-
 const tData = [
     { id: 1, dateTime: "2023-10-01 10:00", type: "Deduct", amount: 100 },
     { id: 2, dateTime: "2023-10-02 11:30", type: "Deduct", amount: 50 },
@@ -87,9 +49,7 @@ function TransactionsPage() {
     const [endIndex, setEndIndex] = useState(rowsPerPage);
 
     useEffect(() => {
-        setTimeout(() => {
-            setData(tData)
-        }, 1000);
+        setData(tData)
     }, [])
 
     return (
@@ -125,7 +85,7 @@ function TransactionsPage() {
                 <PaginationContent>
                     <PaginationItem>
                         <PaginationPrevious href="#"
-                            // className={"pointer-events-none opacity-50"}
+                        // className={"pointer-events-none opacity-50"}
                         />
                     </PaginationItem>
                     <PaginationItem>
@@ -136,7 +96,7 @@ function TransactionsPage() {
                     </PaginationItem>
                     <PaginationItem>
                         <PaginationNext href="#"
-                            // className={"pointer-events-none opacity-50"}
+                        // className={"pointer-events-none opacity-50"}
                         />
                     </PaginationItem>
                 </PaginationContent>
