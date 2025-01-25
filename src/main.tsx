ee36ed18c2af049aa20cbbdae00644f6847ac416
mehul <cs20077@glbitm.ac.in>
2025-01-26 01:55:40 +0530

    refactor: update import paths for LoginForm and Dashboard components

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { HashRouter, Routes, Route } from "react-router";
 import './index.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import UserContextProvider from './context/UserContextProvider.tsx';
-import LoginForm from './app/login/page.tsx';
-import Dashboard from './app/dashboard/page.tsx';
+import LoginForm from './pages/LoginPage.tsx';
+import Dashboard from './pages/Layout.tsx';
 import MembersPage from './pages/MembersPage.tsx'
 import RechargePage from './pages/RechargePage.tsx';
 import TransactionsPage from './pages/TransactionsPage.tsx';
